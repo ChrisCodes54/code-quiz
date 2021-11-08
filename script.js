b21297b6b4d72b465390edfc088a394936ab78b5
ChrisCodes54 <christangarife95@gmail.com>
2021-11-08 17:24:35 -0500

    began creating the js code, started with attempting to create a timer

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 console.log("This is a code quiz")
+var startbtn = document.querySelector(".startbtn").addEventListener("click", function() {
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var timer = document.querySelector("#timer");
+// var secondsleft = 30;
+
+// function countdown() {
+// var timerinterval = setInterval(function() {
+//     secondsleft--;
+//     timer.textContent = "Timer: " + secondsleft;
+    
+//     if(secondsleft === 0) {
+//         clearInterval(timerinterval);
+//         sendmessage();
+//     }
+    
+// }, 1000);
+// }
+
+// function sendmessage() {
+//     timer.textContent = "You lose"
+// }
+
+
+// countdown ();
