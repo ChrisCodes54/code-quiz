102fbd7677f0a5d8c2239c72b3d55b0132f93e09
ChrisCodes54 <christangarife95@gmail.com>
2021-11-10 17:28:25 -0500

    finalized logic functions, needs to be styled, and scores functions needs to be tweaked

---
+++ scores.js
@@ -0,0 +1,13 @@
+// link up with the elements in your .html to render the scores. (getElementByID)
+
+// localStorage.getItem("highscores")
+
+// check out the forEach method to see how to add an <li> to each indivudial score and remember .append()
+
+function printScores(){
+
+
+
+}
+
+printScores();
--- script.js
+++ script.js
@@ -1,93 +1,155 @@
-console.log("This is a code quiz")
-var startbtn = document.querySelector(".startbtn")
+console.log("This is a code quiz");
+var startbtn = document.querySelector(".startbtn");
+var startDiv = document.getElementById("start");
 
-startbtn.addEventListener("click", function() {
-    countdown();
-    hidestarttext();
-    loadquestions();
+startbtn.addEventListener("click", function () {
+  countdown();
+  startDiv.setAttribute("class", "hide");
 
-})
+  loadquestions();
+});
+
+var currentQuestionIndex = 0;
 var timer = document.querySelector(".timernav");
-var secondsleft = 5;
-var starttext = document.getElementById("start-text");
-var shownextbtn = document.getElementsByClassName ("nextbtn");
-var quizq = document.getElementById("questions")
+var secondsleft = 60;
+var shownextbtn = document.getElementsByClassName("nextbtn");
+var quizq = document.getElementById("questions");
+var correct = 0;
+var feedbackDiv = document.getElementById("feedback");
+var timerinterval;
+var finalScore;
+var endQuizDiv = document.getElementById("endQuiz");
 
 
+var myquestions = [
+    {
+      question:
+        "Who in the premier league has gotten the entire season unbeaten?",
+      options: ["Manchester City", "Manchester United", "Chelsea", "Arsenal"],
+      correctAnswer: "Arsenal",
+    },
 
+    {
+      question: "Who has won the most World Cups?",
+      options: ["France", "Brazil", "Germany", "Italy"],
+      correctAnswer: "Arsenal",
+    },
+
+    {
+      question: "Who has played for Barcelona and Real Madrid?",
+      options: ["Messi", "Ronaldo", "Figo", "Pique"],
+      correctAnswer: "Figo",
+    },
+  ];
 //myquestions[0].question //accesing the first object of my questions, and then grabbing the question
 
-function loadquestions () {
-    var firstq = myquestions[0].question;
-    console.log(firstq);
-    var firsto = myquestions[0].options;
-    console.log(firsto);
-    var firsta = myquestions[0].correctAnswer;
-    console.log(firsta);
+function loadquestions() {
+    quizq.removeAttribute("class");
 
-    if(startbtn.style.display === "none"){
-        startbtn.style.display = "block";
-    } else {
-    startbtn.style.display = "none";
-    }
+  var currentQuestion = myquestions[currentQuestionIndex];
+  var questionText = document.getElementById("questiontext")
+  questionText.textContent = currentQuestion.question;
+  var userOptions = document.getElementById("useroptions");
+
+  userOptions.innerHTML = "";
+  
+  // going to the current question and getting the options
+  currentQuestion.options.forEach(function(option, i){
+
+    var optionButton = document.createElement("button");
+    optionButton.setAttribute("value", option);
+    optionButton.setAttribute("class", "optionBtn")
+
+    optionButton.textContent = i + 1 + ". " + option;
+
+    optionButton.onclick = handleClick;
+
+    userOptions.append(optionButton)
+  })
 
 }
 
+function handleClick(){
 
+    if(this.value !== myquestions[currentQuestionIndex].correctAnswer){
+        secondsleft -= 10;
 
+        if(secondsleft < 0){
+            secondsleft = 0;
+        }
 
+        timer.textContent = secondsleft;
 
-
-var myquestions = [ 
-    {
-        question: "Who in the premier league has gotten the entire season unbeaten?",
-        options: ['Manchester City', 'Manchester United', 'Chelsea', 'Arsenal'],
-        correctAnswer: 'Arsenal'
-    },
-
-    {
-        question: "Who has won the most World Cups?",
-        options: ['France','Brazil','Germany','Italy'],
-        correctAnswer: 'Arsenal'
-    },
-
-    {
-        question: "Who has played for Barcelona and Real Madrid?",
-        options: ['Messi','Ronaldo','Figo','Pique'],
-        correctAnswer: 'Figo'
-
+        feedbackDiv.textContent = "Wrong!";
+    } else {
+        feedbackDiv.textContent = "Correct!";
     }
-]
+
+    feedbackDiv.setAttribute("class", "feedback")
+    setTimeout(function(){
+        feedbackDiv.setAttribute("class", "feedback hide")
+    }, 2000);
+
+    currentQuestionIndex++;
+
+    if(currentQuestionIndex === myquestions.length){
+        endQuiz();
+    } else {
+        loadquestions();
+    }
+    correct++;
+    console.log("Correct",correct);
+}
+
+
 function countdown() {
-var timerinterval = setInterval(function() {
+    timerinterval = setInterval(function () {
     secondsleft--;
     timer.textContent = "Timer: " + secondsleft;
-    
-    if(secondsleft === 0) {
-        clearInterval(timerinterval);
-        
+
+    if (secondsleft === 0) {
+      clearInterval(timerinterval);
     }
-    
-}, 1000);
+  }, 1000);
+}
+
+function endQuiz(){
+
+    clearInterval(timerinterval)
+    endQuizDiv.removeAttribute("class");
+
+    var finalScoreSpan = document.getElementById('finalScore');
+
+    var userScore = correct/myquestions.length;
+    var twoDecimals = userScore.toFixed(2);
+    finalScore = twoDecimals.split("0.")[1];
+    console.log(finalScore)
+
+    finalScoreSpan.textContent = finalScore + "%";
+
+}
+
+var submitBtn = document.getElementById('submit');
+submitBtn.onclick = saveScore;
+
+function saveScore(){
+
+    var initialsInput = document.getElementById("initials");
+    var initials = initialsInput.value;
+
+    if(initials !== ""){
+        var highScores = JSON.parse(localStorage.getItem("highscores")) || [];
+    }
+
+    var newScore = {
+        score: finalScore,
+        initials: initials
+    }
+
+    highScores.push(newScore)
+    localStorage.setItem("highscores", JSON.stringify(highScores));
+
+    window.location.href = "highscores.html";
 }
 
 
-function hidestarttext () {
-    var hidestarttext = document.getElementById("start-text")
-    if (hidestarttext.style.display === "none") {
-        hidestarttext.style.display = "block";
-    } else {
-        hidestarttext.style.display = "none";
-    }
-}
-
-
-
-// function buildquiz () {
-    
-// }
-
-// function shownextbtn () {
-//     document.getElementsByClassName("nextbtn").style.display = 'block';
-//     e.preventDefault();
-// }
